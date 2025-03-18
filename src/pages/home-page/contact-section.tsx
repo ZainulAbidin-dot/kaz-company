b971cff4b39eec26574a52a34988bff67acc5b0b
ZainulAbidin-dot <zchxvr@gmail.com>
2025-03-18 04:38:38 +0300

    ...

--- src/pages/home-page/contact-section.tsx
+++ src/pages/home-page/contact-section.tsx
@@ -114,15 +114,15 @@ function ContactInfo() {
             <h2 className="mb-4 text-xl font-bold">Our Location</h2>
             <div className="h-64 w-full">
               <MapContainer
-                center={[51.505, -0.09]}
-                zoom={13}
+                center={[24.676056716523828, 46.66582099537403]}
+                zoom={15}
                 style={{ height: '100%', width: '100%' }}
               >
                 <TileLayer
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={[24.676056716523828, 46.66582099537403]}>
                   <Popup>Our Office Location</Popup>
                 </Marker>
               </MapContainer>
